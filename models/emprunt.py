eb0c2d41254e4c518e5486235c92e2946890edf8
nouryangui <nourelimen.yangui@gmail.com>
2020-12-08 23:05:22 +0100

    verification date emprunt

--- models/emprunt.py
+++ models/emprunt.py
@@ -3,8 +3,8 @@ from odoo import models, fields, api
 
 class BibliothequeEmprunt(models.Model):
     _name = 'bibliotheque.emprunt'
-    date_debut = fields.Date('Date Debut')
-    date_fin = fields.Date('Date Fin')
+    date_debut = fields.Date('Date Emprunte')
+    date_fin = fields.Date('Date Retour')
     livre_id = fields.Many2one(comodel_name='bibliotheque.livre')
     adherent_id = fields.Many2one(comodel_name='bibliotheque.adherent')
 
@@ -14,4 +14,11 @@ class BibliothequeEmprunt(models.Model):
             name = '[' + emprunt.livre_id.titre + ']' + emprunt.adherent_id.nom + ' ' + emprunt.adherent_id.prenom
             result.append((emprunt.id, name))
         return result
-    
+    @api.one
+    @api.constrains('date_debut','date_fin')
+    def checkDate(self):
+        if self.date_fin<self.date_debut:
+            raise ValueError('date début emprunt doit etre inférieur à la fin de l''emprunt')
+
+
+
